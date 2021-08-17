88f824b0838e42f9746b9b3b73faf3fca23bd2ab
Eric <o.eric0521@gmail.com>
2021-08-17 12:42:01 -0400

    made no change just testing stuff  out

--- practice-angular-materials/src/app/app.module.ts
+++ practice-angular-materials/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
+    MatSliderModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
